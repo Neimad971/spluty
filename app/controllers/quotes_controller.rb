80b01a53c80b58e35789fd28e78750bd1083a27f
neimad971 <damientalbot26@gmail.com>
2016-02-18 16:42:24 +0000

    Making the submit button create a new quote

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -11,4 +11,17 @@ class QuotesController < ApplicationController
   end
 
 
+  def create
+  	Quote.create(quote_params)
+  	redirect_to root_path
+  end
+
+
+  private
+
+    def quote_params
+      params.require(:quote).permit(:saying, :author)
+    end
+
+
 end
